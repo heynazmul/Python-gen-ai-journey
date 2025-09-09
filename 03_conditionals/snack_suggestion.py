c21c7db3c0582f8694572c3e2c5373489285958b
heynazmul <heynazmulhossain@gmail.com>
2025-09-09 10:42:33 +0600

    problem -4

--- 03_conditionals/snack_suggestion.py
+++ 03_conditionals/snack_suggestion.py
@@ -5,6 +5,7 @@ If the snack is 'cookies' or 'samosa', it confirms the order.
 Otherwise, it shows that the item is not available.
 """
 
+
 snack = input("Enter Your preferred snack: ").lower()
 
 if snack == "cookies" or snack == "samosa":
---
+++ smart_thermosatat.py
@@ -0,0 +1,19 @@
+"""
+You're building a smart thermostat alert system:
+If the device_status is "active"
+And temperature > 35 → Warn: "High temperature alert!"
+Else: "Temperature normal"
+If device is off "Device is offline
+"""
+
+device_status = "active"
+
+temprature = 38
+
+if device_status == "active":
+    if temprature >35:
+        print("High temperature alart! 🔥")
+    else:
+        print("Tempreature is normal  😊")
+else: 
+    print("Device is offline")
